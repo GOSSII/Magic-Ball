a1099fed7ffa11b41fe317ecd8eed757c701cf50
Dipesh Goswami <dipeshpuri9@gmail.com>
2020-01-01 16:42:13 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.red,
+        ),
+        //body: DicePage(),
+      ),
+    ),
+  );
+}
