d8665308cd06a3171cd09aa4d80f2ce95e0f7440
Dipesh Goswami <dipeshpuri9@gmail.com>
2020-01-01 16:49:50 -0500

    Completed Magic Ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.lightBlue,
         appBar: AppBar(
           title: Text('Ask Me Anything'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blue,
         ),
-        //body: DicePage(),
+        body: Ball(),
       ),
     ),
   );
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+
+class _BallState extends State<Ball> {
+  int leftDiceNumber = 1;
+
+  void returnDiceFace(String face){
+    setState(() {
+      leftDiceNumber = Random().nextInt(5) +1;
+      print('$face button got Pressed. $leftDiceNumber');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                returnDiceFace("Left");
+              },
+              child: Image.asset('images/ball$leftDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
